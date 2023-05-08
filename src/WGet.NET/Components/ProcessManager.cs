ad29daea8fccdda05bc3459a7f5d3a7caad9fa0f
Bjarne <59922570+basicx-StrgV@users.noreply.github.com>
2023-05-08 18:19:25 +0200

    Added asynchronous execution to the ProcessManager class

--- src/WGet.NET/Components/ProcessManager.cs
+++ src/WGet.NET/Components/ProcessManager.cs
@@ -5,6 +5,7 @@
 using System.IO;
 using System.Text;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using WGetNET.HelperClasses;
 
 namespace WGetNET
@@ -15,7 +16,7 @@ namespace WGetNET
     /// </summary>
     internal class ProcessManager
     {
-        private readonly ProcessStartInfo _winGetStartInfo;
+        private readonly ProcessStartInfo _winGetStartInfoTemplate;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WGetNET.ProcessManager"/> class.
@@ -25,7 +26,7 @@ namespace WGetNET
         /// </param>
         public ProcessManager(string processName)
         {
-            _winGetStartInfo = new ProcessStartInfo()
+            _winGetStartInfoTemplate = new ProcessStartInfo()
             {
                 CreateNoWindow = true,
                 FileName = processName,
@@ -46,10 +47,45 @@ namespace WGetNET
         /// </returns>
         public ProcessResult ExecuteWingetProcess(string cmd)
         {
-            //Set Arguments
-            _winGetStartInfo.Arguments = cmd;
+            return RunProcess(GetStartInfo(cmd));
+        }
 
-            return RunProcess();
+        /// <summary>
+        /// Asynchronous executes a winget process with the given command and returns the result.
+        /// </summary>
+        /// <param name="cmd">
+        /// A <see cref="System.String"/> representing the command that winget should be executed with.
+        /// </param>
+        /// <returns>
+        /// A <see cref="WGetNET.ProcessResult"/> object, 
+        /// containing the output an exit id of the process.
+        /// </returns>
+        public async Task<ProcessResult> ExecuteWingetProcessAsync(string cmd)
+        {
+            return await RunProcessAsync(GetStartInfo(cmd));
+        }
+
+        /// <summary>
+        /// Gets the start info for a process.
+        /// </summary>
+        /// <param name="cmd">
+        /// String containig the arguments for the action.
+        /// </param>
+        /// <returns>
+        /// A <see cref="System.Diagnostics.ProcessStartInfo"/> object, for the process.
+        /// </returns>
+        private ProcessStartInfo GetStartInfo(string cmd)
+        {
+            return new ProcessStartInfo()
+            {
+                CreateNoWindow = _winGetStartInfoTemplate.CreateNoWindow,
+                FileName = _winGetStartInfoTemplate.FileName,
+                RedirectStandardOutput = _winGetStartInfoTemplate.RedirectStandardOutput,
+                StandardOutputEncoding = _winGetStartInfoTemplate.StandardOutputEncoding,
+                UseShellExecute = _winGetStartInfoTemplate.UseShellExecute,
+                WindowStyle = _winGetStartInfoTemplate.WindowStyle,
+                Arguments = cmd
+            };
         }
 
         /// <summary>
@@ -59,16 +95,42 @@ namespace WGetNET
         /// A <see cref="WGetNET.ProcessResult"/> object, 
         /// containing the output an exit id of the process.
         /// </returns>
-        private ProcessResult RunProcess()
+        private ProcessResult RunProcess(ProcessStartInfo processStartInfo)
         {
             ProcessResult result = new ProcessResult();
 
             //Create and run process
-            using (Process proc = new Process { StartInfo = _winGetStartInfo })
+            using (Process proc = new Process { StartInfo = processStartInfo })
             {
                 proc.Start();
 
                 result.Output = ReadSreamOutput(proc.StandardOutput);
+
+                //Wait till end and get exit code
+                proc.WaitForExit();
+                result.ExitCode = proc.ExitCode;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Asynchronous runs a process with the current start informations.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="WGetNET.ProcessResult"/> object, 
+        /// containing the output an exit id of the process.
+        /// </returns>
+        private async Task<ProcessResult> RunProcessAsync(ProcessStartInfo processStartInfo)
+        {
+            ProcessResult result = new ProcessResult();
+
+            //Create and run process
+            using (Process proc = new Process { StartInfo = processStartInfo })
+            {
+                proc.Start();
+
+                result.Output = await ReadSreamOutputAsync(proc.StandardOutput);
 
                 //Wait till end and get exit code
                 proc.WaitForExit();
@@ -96,6 +158,35 @@ namespace WGetNET
             while (!output.EndOfStream)
             {
                 string? outputLine = output.ReadLine();
+                if (outputLine is null)
+                {
+                    continue;
+                }
+
+                outputArray = ArrayManager.Add(outputArray, outputLine);
+            }
+
+            return outputArray;
+        }
+
+        /// <summary>
+        /// Asynchronous reads the data from the process output to a string array.
+        /// </summary>
+        /// <param name="output">
+        /// The <see cref="System.IO.StreamReader"/> with the process output.
+        /// </param>
+        /// <returns>
+        /// A <see cref="System.String"/> array 
+        /// containing the process output stream content by lines.
+        /// </returns>
+        private async Task<string[]> ReadSreamOutputAsync(StreamReader output)
+        {
+            string[] outputArray = new string[0];
+
+            //Read output to list
+            while (!output.EndOfStream)
+            {
+                string? outputLine = await output.ReadLineAsync();
                 if (outputLine is null)
                 {
                     continue;
