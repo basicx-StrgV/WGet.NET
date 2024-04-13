e6d0c22d5c5a4208208a32894cd6f9f77def850e
Bjarne <basicx.strgv@gmail.com>
2024-04-13 23:26:38 +0200

    Release 4.1.0 build

--- docs/search/all_10.js
+++ docs/search/all_10.js
@@ -3,9 +3,11 @@ var searchData=
   ['rawcontent_0',['RawContent',['../de/d76/a00134.html#aa99d45ad14609c66115794e0d5eadf77',1,'WGetNET::Abstractions::WinGetInfoEntry']]],
   ['removesources_1',['RemoveSources',['../d7/d25/a00182.html#a5ec3a8384a0dc7156933063238018c8b',1,'WGetNET.WinGetSourceManager.RemoveSources(string name)'],['../d7/d25/a00182.html#a25656610b4a23db99f57862a9eea990b',1,'WGetNET.WinGetSourceManager.RemoveSources(WinGetSource source)']]],
   ['removesourcesasync_2',['RemoveSourcesAsync',['../d7/d25/a00182.html#a0595104988e004ad17682e34fc9ac450',1,'WGetNET.WinGetSourceManager.RemoveSourcesAsync(string name, CancellationToken cancellationToken=default)'],['../d7/d25/a00182.html#ac018b78ee454403ecf750acd25f13897',1,'WGetNET.WinGetSourceManager.RemoveSourcesAsync(WinGetSource source, CancellationToken cancellationToken=default)']]],
-  ['requirements_3',['❗ Requirements',['../index.html#autotoc_md6',1,'']]],
-  ['resetpins_4',['ResetPins',['../d9/d90/a00178.html#a7d14399ecd95c422e86aec8d21aaa05f',1,'WGetNET::WinGetPackageManager']]],
-  ['resetpinsasync_5',['ResetPinsAsync',['../d9/d90/a00178.html#a76e12753d2cbe1b9629b2ef247ae6dc6',1,'WGetNET::WinGetPackageManager']]],
-  ['resetsources_6',['ResetSources',['../d7/d25/a00182.html#aea125a25b6ab9f169e6d9c605be31f82',1,'WGetNET::WinGetSourceManager']]],
-  ['resetsourcesasync_7',['ResetSourcesAsync',['../d7/d25/a00182.html#acd994dd471858134a7f2835a6689353b',1,'WGetNET::WinGetSourceManager']]]
+  ['repairpackage_3',['RepairPackage',['../d9/d90/a00178.html#a2d69b650aa8bbf56e18e407ae0668238',1,'WGetNET.WinGetPackageManager.RepairPackage(string packageId)'],['../d9/d90/a00178.html#a1c2329513e04238450a9ad17cdd0c638',1,'WGetNET.WinGetPackageManager.RepairPackage(WinGetPackage package)']]],
+  ['repairpackageasync_4',['RepairPackageAsync',['../d9/d90/a00178.html#a0f280a08a9fa9b6cbfd6cadd3cfe311d',1,'WGetNET.WinGetPackageManager.RepairPackageAsync(string packageId, CancellationToken cancellationToken=default)'],['../d9/d90/a00178.html#a11f0bbf6d597902167dadad9ec257847',1,'WGetNET.WinGetPackageManager.RepairPackageAsync(WinGetPackage package, CancellationToken cancellationToken=default)']]],
+  ['requirements_5',['❗ Requirements',['../index.html#autotoc_md6',1,'']]],
+  ['resetpins_6',['ResetPins',['../d9/d90/a00178.html#a7d14399ecd95c422e86aec8d21aaa05f',1,'WGetNET::WinGetPackageManager']]],
+  ['resetpinsasync_7',['ResetPinsAsync',['../d9/d90/a00178.html#a76e12753d2cbe1b9629b2ef247ae6dc6',1,'WGetNET::WinGetPackageManager']]],
+  ['resetsources_8',['ResetSources',['../d7/d25/a00182.html#aea125a25b6ab9f169e6d9c605be31f82',1,'WGetNET::WinGetSourceManager']]],
+  ['resetsourcesasync_9',['ResetSourcesAsync',['../d7/d25/a00182.html#acd994dd471858134a7f2835a6689353b',1,'WGetNET::WinGetSourceManager']]]
 ];
--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -14,5 +14,6 @@ var searchData=
   ['isempty_11',['IsEmpty',['../de/d76/a00134.html#a95be57faed890b8d568942d2920f9e71',1,'WGetNET.Abstractions.WinGetInfoEntry.IsEmpty'],['../dc/d57/a00194.html#a94ba6e82a7088bb1339a427e249478cf',1,'WGetNET.WinGetInfo.IsEmpty'],['../db/d53/a00202.html#a2e52f2a27eecc0d0053d08c40f70b22d',1,'WGetNET.WinGetPackage.IsEmpty'],['../da/de7/a00210.html#a27474cab3379b9d20a921827456f18d8',1,'WGetNET.WinGetSource.IsEmpty'],['../d2/dc8/a00242.html#abb8bacf343d16d4203e81dc2aa5f3a9d',1,'WGetNET.IWinGetObject.IsEmpty']]],
   ['isenabled_12',['IsEnabled',['../d8/d00/a00186.html#abc64d69de0274c17c4c10873adaf84ed',1,'WGetNET::WinGetAdminSetting']]],
   ['isinstalled_13',['IsInstalled',['../d8/df1/a00174.html#a12db89d97980f50f44668f34f0db1ca3',1,'WGetNET::WinGet']]],
-  ['iwingetobject_14',['IWinGetObject',['../d2/dc8/a00242.html',1,'WGetNET']]]
+  ['ispreview_14',['IsPreview',['../d8/df1/a00174.html#a0c3fb7499a18595abf999337b6486924',1,'WGetNET::WinGet']]],
+  ['iwingetobject_15',['IWinGetObject',['../d2/dc8/a00242.html',1,'WGetNET']]]
 ];
